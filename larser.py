693b4adda9a0fa32eecbbadb2e50a4f54c2cc90d
pumpkinpai <pumpkinson@gmail.com>
2015-06-22 12:32:39 -0400

    werk werk werk

--- larser.py
+++ larser.py
@@ -12,35 +12,43 @@ def getConf(filename):
     return conf
 
 def fileChooser(filetype):
-    fileList = []
-    curDir = os.getcwd()
-    for f in os.listdir(curDir):
-        if f.endswith(filetype):
-            fileList.append(f)
-    fileList.sort()
+    chosen = -1
+    while chosen == -1:
+        fileList = []
+        curDir = os.getcwd()
+        for f in os.listdir(curDir):
+            if f.endswith(filetype):
+                fileList.append(f)
+        fileList.sort()
 
-    menu = ''
-    index = 0
-    for element in fileList:
-        menu += str(index) + '- ' + element + '\n'
-        index += 1
+        menu = ''
+        index = 0
+        for element in fileList:
+            menu += str(index) + '- ' + element + '\n'
+            index += 1
 
-    menu += '\nSelect a conf file number: '
+        menu += '\nSelect a conf file number: '
 
-    userSelection = input(menu) 
+        userSelection = input(menu) 
 
-    chosen = fileList[int(userSelection)]
-    return chosen
+        # Avoid index errors
+        if int(userSelection) <= 1 and userSelection != '': # fileList.len():  todo: use proper list length function
+            chosen = fileList[int(userSelection)]
+            return chosen
+        else:
+            print('Invalid input.  Please try again.\n')
+            chosen = -1
 
 
 if __name__ == '__main__':
-    confFileName = 'defaukt.yaml'
+    confFilename = 'defaukt.yaml'
 
     # todo Set source file from sys arg
+    sourceFilename = 'temp.jpg'
 
     # todo Ask if default values from conf.yaml
     confFilename = fileChooser('.yaml')
-    print(confFilename)
+    print(confFilename + ' selected')
 
     if confFilename[0].lower() == 'n':
         pass
